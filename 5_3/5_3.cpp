fe798d05da274b52953c68e8e1779ac1affc4ddc
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-03-27 14:14:24 +0800

    5_3

--- 5_3/5_3.cpp
+++ 5_3/5_3.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv.hpp>
+using namespace cv;
 
 int main()
 {
-    return 0;
-}
+	VideoCapture cap;
 
+	cap.open(0);
+
+	if (!cap.isOpened())
+	{
+		std::cout << "不能打开视频文件" << std::endl;
+		return-1;
+	}
+
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat dst;
+		bool rSucess = cap.read(frame);
+		if (!rSucess)
+		{
+			std::cout << "不能从视频文件中读取帧" << std::endl;
+			break;
+		}
+		else
+		{
+			GaussianBlur(frame, dst, Size(5, 5),5,0);
+			cv::imshow("原图", frame);
+			cv::imshow("高斯滤波", dst);
+		}
+		waitKey(30);
+	}
+}
